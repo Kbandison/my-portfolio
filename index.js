a44ba8d2051c510332fd89fd892e941c9d4ff14c
Kevin Bandison <kbandison@gmail.com>
2022-11-27 07:05:45 -0500

    added more style and a dark mode button

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ let slider = document.querySelector("#switch-btn");
 
 let banner = document.querySelector("#title");
 
+let mobileSwitch = document.querySelector(".mobile-switch");
+
+let mobileButton = document.querySelector(".mbl-btn");
+
+let mobileSwitchLabel = document.querySelector(".mobile-switch label");
+
 button.addEventListener("click", () => {
   button.classList.toggle("dark");
 
@@ -22,5 +28,26 @@ button.addEventListener("click", () => {
     page.style.transition = "200ms ease-in-out";
     button.style.backgroundColor = "#efecec";
     banner.style.color = "#6017e3";
+  }
+});
+
+mobileSwitch.addEventListener("click", () => {
+  mobileSwitch.classList.toggle("dark");
+
+  if (mobileSwitch.classList.contains("dark")) {
+    page.style.backgroundColor = "rgba(10, 10, 10)";
+    page.style.transition = "200ms ease-in-out";
+    button.style.backgroundColor = "rgba(30,30,30)";
+    slider.style.borderColor = "#6017e3";
+    banner.style.color = "white";
+    mobileSwitchLabel.innerText = "Light Mode";
+    mobileButton.style.backgroundColor = "white";
+  } else {
+    page.style.backgroundColor = "rgba(255, 255, 255)";
+    page.style.transition = "200ms ease-in-out";
+    button.style.backgroundColor = "#efecec";
+    banner.style.color = "#6017e3";
+    mobileSwitchLabel.innerText = "Dark Mode";
+    mobileButton.style.backgroundColor = "rgba(10,10,10)";
   }
 });
